80916fef0e2b4417acef3780dec2b0903653fa9d
Daniel Bannert <prisis@users.noreply.github.com>
2017-06-20 23:34:22 +0200

    Fix proxies and use nulllogger (#592)

--- Proxy/Log.php
+++ Proxy/Log.php
@@ -1,7 +1,8 @@
 <?php
 declare(strict_types=1);
-namespace Viserio\Component\Log\Proxie;
+namespace Viserio\Component\Log\Proxy;
 
+use Viserio\Component\Log\Writer as MonologWriter;
 use Viserio\Component\StaticalProxy\StaticalProxy;
 
 class Log extends StaticalProxy
@@ -13,6 +14,6 @@ class Log extends StaticalProxy
      */
     public static function getInstanceIdentifier()
     {
-        return 'logger';
+        return MonologWriter::class;
     }
 }
